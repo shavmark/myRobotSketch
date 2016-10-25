a203654007664b685c4cb38dc0ca7e25c37686ef
mark <mark@shavlik.us>
2016-10-25 10:55:23 -0500

    tweaked header

--- src/ofRobots.h
+++ src/ofRobots.h
@@ -1,19 +1,19 @@
 /*
 ofRobots.h - openframeworks based classes for managing robots
-Copyright (c) 2016 Mark J Shavlik.  All right reserved.This file is part of myRobot.
+Copyright (c) 2016 Mark J Shavlik.  All right reserved.This file is part of myRobotSketch.
 
-myRobot is free software : you can redistribute it and / or modify
+myRobotSketch is free software : you can redistribute it and / or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-myRobot is distributed in the hope that it will be useful,
+myRobotSketch is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with myRobot.If not, see <http://www.gnu.org/licenses/>.
+along with myRobotSketch.If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
